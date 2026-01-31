f7ffbbbe2b8096a349aaa46ceaf7b06b103d453d
nuocngoaihoaky-gif <nuocngoaihoaky@gmail.com>
2026-01-31 12:44:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -355,6 +355,10 @@ def main():
                 # Refresh nhẹ để giữ session, không làm gì khác
                 driver.get("https://m.facebook.com/?locale=en_US")
                 time.sleep(10)
+                gui_anh_tele(
+                    driver,
+                    "✅ VẪN SỐNG" + gps_log
+                )
             except: pass
 
         print(">>> ✅ XONG 6 TIẾNG.", flush=True)
